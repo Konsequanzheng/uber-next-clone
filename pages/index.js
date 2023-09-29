f649db2a7fd529ccd0b9781a58fa1a0cb62b0c44
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-09-29 20:59:50 +0300

    Add ActionButtons

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import tw from 'tailwind-styled-components'
 import mapboxgl from 'mapbox-gl'
 import Map from './components/Map'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,16 +15,77 @@ export default function Home() {
     <Wrapper>
       <Map />
       <ActionItems>
-        
+        {/* Header */}
+        <Header>
+          <UberLogo src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/800px-Uber_logo_2018.svg.png' />
+          <Profile>
+            <Name>Quan Zheng</Name>
+            <UserImage src='https://media.licdn.com/dms/image/D4D03AQFsasCWgt7nSw/profile-displayphoto-shrink_800_800/0/1670387506050?e=1701302400&v=beta&t=dFW_ogWNcPoFMMPaNfZSuo4xCrVxlhq8vUQ2aEOHoAU'/>
+          </Profile>
+        </Header>
+        {/* ActionButtons */}
+        <ActionButtons>
+          <Link href='search/'>
+            <ActionButton>
+              <ActionButtonImage src='https://i.ibb.co/cyvcpfF/uberx.png'/>
+              Ride
+            </ActionButton>
+          </Link>
+          <ActionButton>
+          <ActionButtonImage src='https://i.ibb.co/n776JLm/bike.png'/>
+            Wheels
+          </ActionButton>
+          <ActionButton>
+          <ActionButtonImage src='https://i.ibb.co/5RjchBg/uberschedule.png'/>
+            Reserve
+          </ActionButton>
+        </ActionButtons>
+        <InputButton> Where to?</InputButton>
+        {/* InputBUtton */}
       </ActionItems>
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen
+  flex flex-col h-screen
 `
 
 const ActionItems = tw.div`
-  flex-1
+  flex-1 p-4
+`
+
+const Header = tw.div`
+  flex justify-between items-center
+`
+const Profile = tw.div`
+  flex items-center
+`
+
+const Name = tw.div`
+  mr-4 w-20 text-sm
+`
+
+const UberLogo = tw.img`
+  h-24 p-5
+`
+
+const UserImage = tw.img`
+  h-12 w-12 rounded-full border border-gray-200 p-px
+`
+
+const ActionButtons = tw.div`
+  flex
+`
+
+const ActionButton = tw.div`
+  flex flex-col bg-gray-200 flex-1 m-1 h-32 items-center justify-center rounded-lg transform hover:scale-105 transition text-xl
+`
+
+const ActionButtonImage = tw.img`
+  h-3/5
+`
+
+const InputButton = tw.div`
+  h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
 `
