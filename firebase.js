b74e0d7826e3465ea5c45e4e727b99ab187848d3
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-09-30 19:41:36 +0300

    Start adding firebase

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider}
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIntLWGCuri4t4cUEv8fPvNu2fztfsjk4",
+  authDomain: "uber-next-clone-780d0.firebaseapp.com",
+  projectId: "uber-next-clone-780d0",
+  storageBucket: "uber-next-clone-780d0.appspot.com",
+  messagingSenderId: "620713685524",
+  appId: "1:620713685524:web:f8d20e80451cb1e8ccad01"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
